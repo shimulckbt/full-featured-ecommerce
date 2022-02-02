c5a0117a97d560e375c9441deef784b6ae864327
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-02-02 19:13:50 +0600

    image preview javascript added

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -2,6 +2,8 @@
 
 @section('main_content')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <div class="container-full">
 
    <!-- Main content -->
@@ -52,14 +54,14 @@
                                  <div class="form-group">
                                     <h5>profile Image <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                       <input type="file" name="profile_photo_path" class="form-control" required="">
-                                       <div class="help-block"></div>
+                                       <input type="file" name="profile_photo_path" class="form-control" required="" id="image">
+                                       <!-- <div class="help-block"></div> -->
                                     </div>
                                  </div>
                               </div>
 
                               <div class="col-md-5">
-                                 <img style="width: 100px;height: 100px;" src=" {{(!empty($adminData->profile_photo_path))?url('upload/admin_images/'.$adminData->profile_photo_path):url('upload/no_image.jpg')}}" alt="User Avater">
+                                 <img id="showImage" style="width: 100px;height: 100px;" src=" {{(!empty($adminData->profile_photo_path))?url('upload/admin_images/'.$adminData->profile_photo_path):url('upload/no_image.jpg')}}" alt="User Avater">
                               </div>
 
                            </div>
@@ -81,5 +83,17 @@
    </section>
    <!-- /.content -->
 </div>
+
+<script type="text/javascript">
+   $(document).ready(function() {
+      $('#image').change(function(e) {
+         var reader = new FileReader();
+         reader.onload = function(e) {
+            $('#showImage').attr('src', e.target.result);
+         }
+         reader.readAsDataURL(e.target.files['0']);
+      });
+   });
+</script>
 
 @endsection
