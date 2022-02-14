e5995306fe2163ddf6a8347212866571dc1a477d
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-02-14 21:28:24 +0600

    user profile update with image

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,12 +3,53 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class IndexController extends Controller
 {
     public function index()
     {
         return view('user.index');
+    }
+
+    public function userLogout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function userProfile()
+    {
+        $id = Auth::user()->id;
+        $user = User::findOrFail($id);
+        return view('user.profile.show', compact('user'));
+    }
+
+    public function userProfileUpdate(Request $request)
+    {
+        $id = Auth::user()->id;
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+
+        if ($request->file('profile_photo_path')) {
+            $file = $request->file('profile_photo_path');
+            @unlink(public_path('upload/user_images/' . $user['profile_photo_path']));
+
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+
+            $file->move(public_path('upload/user_images'), $filename);
+            $user['profile_photo_path'] = $filename;
+        }
+        $user->save();
+
+        $notification = array(
+            'message' => 'User Profile Updated Successfully..!',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('user.profile')->with($notification);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\AdminProfileController;
 use App\Http\Controllers\Frontend\IndexController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,25 +28,40 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function ()
     Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
 });
 
-Route::middleware(['auth:admin'])->group(function () {
-    Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
-        return view('admin.index');
-    })->name('dashboard');
+Route::group(
+    ['prefix' => '/admin', 'middleware' => ['auth:admin']],
+    function () {
+        Route::middleware(['auth:sanctum,admin', 'verified'])->get('/dashboard', function () {
+            return view('admin.index');
+        })->name('dashboard');
 
 
-    Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
-    Route::get('/admin/profile', [AdminProfileController::class, 'adminProfile'])->name('admin.profile');
-    Route::get('/admin/profile/edit', [AdminProfileController::class, 'adminProfileEdit'])->name('admin.profile.edit');
-    Route::post('/admin/profile/update', [AdminProfileController::class, 'adminProfileUpdate'])->name('admin.profile.update');
-    Route::get('/admin/change/password', [AdminProfileController::class, 'adminChangePassword'])->name('admin.change.password');
-    Route::post('/admin/update/password', [AdminProfileController::class, 'adminUpdatePassword'])->name('admin.update.password');
-});
+        Route::get('/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+        Route::get('/profile', [AdminProfileController::class, 'adminProfile'])->name('admin.profile');
+        Route::get('/profile/edit', [AdminProfileController::class, 'adminProfileEdit'])->name('admin.profile.edit');
+        Route::post('/profile/update', [AdminProfileController::class, 'adminProfileUpdate'])->name('admin.profile.update');
+        Route::get('/change/password', [AdminProfileController::class, 'adminChangePassword'])->name('admin.change.password');
+        Route::post('/update/password', [AdminProfileController::class, 'adminUpdatePassword'])->name('admin.update.password');
+    }
+);
 
 
 /////          User All Route          /////
 
-Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
 
 Route::get('/', [IndexController::class, 'index']);
+
+Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
+    $id = Auth::user()->id;
+    $user = User::findOrFail($id);
+    return view('dashboard', compact('user'));
+})->name('dashboard');
+
+Route::group(
+    ['prefix' => '/user', 'middleware' => ['auth:web']],
+    function () {
+        Route::get('/logout', [IndexController::class, 'userLogout'])->name('user.logout');
+        Route::get('/profile', [IndexController::class, 'userProfile'])->name('user.profile');
+        Route::post('/profile/update', [IndexController::class, 'userProfileUpdate'])->name('user.profile.update');
+    }
+);
