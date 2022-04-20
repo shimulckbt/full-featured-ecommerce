a7f66ba25a89d710349afc7d2d1e54af0378dfb7
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-20 15:12:31 +0600

    remo

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,6 @@ use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Models\User;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
