04da099ba8a4ee107111eaf233d5a36733c191af
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-25 23:42:14 +0600

    edited inex blade file

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -217,13 +217,50 @@
                                  <a href="#" class="waves-effect waves-light btn btn-info btn-circle mx-5"><span class="mdi mdi-arrow-right"></span></a>
                               </td>
                            </tr>
-                           <td>
-                              <span class="badge badge-danger-light badge-lg">Rejected</span>
-                           </td>
-                           <td class="text-right">
-                              <a href="#" class="waves-effect waves-light btn btn-info btn-circle mx-5"><span class="mdi mdi-bookmark-plus"></span></a>
-                              <a href="#" class="waves-effect waves-light btn btn-info btn-circle mx-5"><span class="mdi mdi-arrow-right"></span></a>
-                           </td>
+                           <tr>
+                              <td class="pl-0 py-8">
+                                 <div class="d-flex align-items-center">
+                                    <div class="flex-shrink-0 mr-20">
+                                       <div class="bg-img h-50 w-50" style="background-image: url(../images/gallery/creative/img-4.jpg)"></div>
+                                    </div>
+
+                                    <div>
+                                       <a href="#" class="text-white font-weight-600 hover-primary mb-1 font-size-16">Vivamus consectetur</a>
+                                       <span class="text-fade d-block">Pharetra, Nulla , Nec, Aliquet</span>
+                                    </div>
+                                 </div>
+                              </td>
+                              <td>
+                                 <span class="text-fade font-weight-600 d-block font-size-16">
+                                    Paid
+                                 </span>
+                                 <span class="text-white font-weight-600 d-block font-size-16">
+                                    $45,800k
+                                 </span>
+                              </td>
+                              <td>
+                                 <span class="text-fade font-weight-600 d-block font-size-16">
+                                    Paid
+                                 </span>
+                                 <span class="text-white font-weight-600 d-block font-size-16">
+                                    $45k
+                                 </span>
+                              </td>
+                              <td>
+                                 <span class="text-fade font-weight-600 d-block font-size-16">
+                                    Sophia
+                                 </span>
+                                 <span class="text-white font-weight-600 d-block font-size-16">
+                                    Pharetra
+                                 </span>
+                              </td>
+                              <td>
+                                 <span class="badge badge-danger-light badge-lg">Rejected</span>
+                              </td>
+                              <td class="text-right">
+                                 <a href="#" class="waves-effect waves-light btn btn-info btn-circle mx-5"><span class="mdi mdi-bookmark-plus"></span></a>
+                                 <a href="#" class="waves-effect waves-light btn btn-info btn-circle mx-5"><span class="mdi mdi-arrow-right"></span></a>
+                              </td>
                            </tr>
                            <tr>
                               <td class="pl-0 py-8">
