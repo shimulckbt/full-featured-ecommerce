032ca0dcfd31855f01c4545591a17e9367a32307
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-27 20:21:09 +0600

    another table

--- resources/views/admin/order/order.blade.php
+++ resources/views/admin/order/order.blade.php
@@ -16,7 +16,32 @@
                <!-- /.box-header -->
                <div class="box-body">
                   <div class="table-responsive">
-
+                     <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                           <tr>
+                              <th>SL</th>
+                              <th>Category En</th>
+                              <th>Category Bn</th>
+                              <th>Icon</th>
+                              <th>Action</th>
+                           </tr>
+                        </thead>
+                        <tbody>
+                           @php($i=1)
+                           @foreach($categories as $category)
+                           <tr>
+                              <td>{{$i++}}</td>
+                              <td>{{$category->category_name_en}}</td>
+                              <td>{{$category->category_name_bn}}</td>
+                              <td><img src="{{asset($category->category_icon)}}" style="height: 40px; width:60;" alt="no icon"></td>
+                              <td>
+                                 <a href="{{route('edit.category',$category->id)}}" class="btn btn-info btn-md" title="Edit Data"><i class="fa fa-pencil"></i></a>
+                                 <a href="{{route('delete.category',$category->id)}}" class="btn btn-danger btn-md" title="Delete Data" id="delete"><i class="fa fa-trash-o"></i></a>
+                              </td>
+                           </tr>
+                           @endforeach
+                        </tbody>
+                     </table>
                   </div>
                </div>
                <!-- /.box-body -->
