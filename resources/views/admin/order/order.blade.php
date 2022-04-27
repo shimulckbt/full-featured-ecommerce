07d75a245430a906c16b7bce09aa07466658614b
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-27 20:21:31 +0600

    form added

--- resources/views/admin/order/order.blade.php
+++ resources/views/admin/order/order.blade.php
@@ -60,7 +60,41 @@
                <!-- /.box-header -->
                <div class="box-body">
                   <div class="table-responsive">
+                     <form method="post" action="{{route('create.category')}}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                           <h5>Category Name in English <span class="text-danger">*</span></h5>
+                           <div class="controls">
+                              <input type="text" id="category_name_en" name="category_name_en" class="form-control" required>
+                              @error('category_name_en')
+                              <span class="text-danger">{{$message}}</span>
+                              @enderror
+                           </div>
+                        </div>
 
+                        <div class="form-group">
+                           <h5>Category Name in Mother Language <span class="text-danger">*</span></h5>
+                           <div class="controls">
+                              <input type="text" id="category_name_bn" name="category_name_bn" class="form-control" required>
+                              @error('category_name_bn')
+                              <span class="text-danger">{{$message}}</span>
+                              @enderror
+                           </div>
+                        </div>
+
+                        <div class="form-group">
+                           <h5>category icon <span class="text-danger">*</span></h5>
+                           <div class="controls">
+                              <input type="file" id="category_icon" name="category_icon" class="form-control" required>
+                              @error('category_icon')
+                              <span class="text-danger">{{$message}}</span>
+                              @enderror
+                           </div>
+                        </div>
+                        <div class=" text-xs-right">
+                           <input type="submit" class="btn btn-rounded btn-info btn-md" value="Add New">
+                        </div>
+                     </form>
                   </div>
                </div>
                <!-- /.box-body -->
