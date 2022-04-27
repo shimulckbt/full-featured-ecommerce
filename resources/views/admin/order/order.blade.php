fbc6495e62536b543a7462b07c43786e38030ada
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-27 20:20:49 +0600

    normal design

--- resources/views/admin/order/order.blade.php
+++ resources/views/admin/order/order.blade.php
@@ -3,5 +3,51 @@
 @section('main_content')
 
 
+<div class="container-full">
+   <!-- Main content -->
+   <section class="content">
+      <div class="row">
+         <div class="col-8">
 
+            <div class="box">
+               <div class="box-header with-border">
+                  <h3 class="box-title">All Categories</h3>
+               </div>
+               <!-- /.box-header -->
+               <div class="box-body">
+                  <div class="table-responsive">
+
+                  </div>
+               </div>
+               <!-- /.box-body -->
+            </div>
+
+         </div>
+
+         <!-- add category section -->
+
+         <div class="col-4">
+
+            <div class="box">
+               <div class="box-header with-border">
+                  <h3 class="box-title">Add category</h3>
+               </div>
+               <!-- /.box-header -->
+               <div class="box-body">
+                  <div class="table-responsive">
+
+                  </div>
+               </div>
+               <!-- /.box-body -->
+            </div>
+
+         </div>
+
+         <!-- add category section end -->
+      </div>
+      <!-- /.row -->
+   </section>
+   <!-- /.content -->
+
+</div>
 @endsection
